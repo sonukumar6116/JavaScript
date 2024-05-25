59f024393f92922ba096ee2d0eb5de454c4f18d9
Sonu Kumar <109718616+sonukumar6116@users.noreply.github.com>
2024-05-25 09:00:09 +0530

    Inheritance

---
+++ 21_Inheritance_Call.js
@@ -0,0 +1,31 @@
+class User{
+      constructor(name,email){
+            this.name=name;
+            this.email=email
+      }
+
+      UserMethod(){
+            console.log(this)  
+      }
+}
+
+class Teacher extends User{
+      constructor(name,email,Proffesion){
+            super(name,email)
+            this.Proffesion=Proffesion
+      }
+
+      TeacherMethod(){
+            console.log(this) 
+      }
+}
+
+const  Teacher1 = new Teacher("sonu","@gmail","CSE")
+const  user1 = new User("monu","@email")
+
+// console.log(Teacher1)       // Teacher { name: 'sonu', email: '@gmail', Proffesion: 'CSE' }
+// Teacher1.TeacherMethod()    // Teacher { name: 'sonu', email: '@gmail', Proffesion: 'CSE' }
+// user1.UserMethod()          // User { name: 'monu', email: '@email' }
+
+// Teacher1.UserMethod()      // Teacher { name: 'sonu', email: '@gmail', Proffesion: 'CSE' }
+// user1.TeacherMethod()     // ERROR NOT A FUNCTION
